abd0b6acc8b742bb4ed89b2feaf5951160068539
WenQiang <WenQiangW@users.noreply.github.com>
2017-04-03 11:35:51 +0800

    Update SelectSort.h

--- SelectSort.h
+++ SelectSort.h
@@ -1,14 +1,15 @@
 #include<iostream>
 
-//ֱ��ѡ�������ҵ����ֵ����Сֵ�� �ŵ����һ��
-//���ȶ���
+
+//直接选择排序：找到最大值（最小值） 放到最后一个
+//不稳定的
 void SelectionSort(int arr[],size_t size)
 {
-	for (size_t idx = 0; idx < size -1; ++idx) //�������� ,��һ�����������һ�� ��һ��Ԫ��
+	for (size_t idx = 0; idx < size -1; ++idx) //控制趟树 ,少一次趟树，最后一趟 就一个元素
 	{
 		int maxpos = 0;
 		int find = arr[maxpos];
-		for (size_t i = 1; i < size - idx; ++i) //�����ֵ����Сֵ
+		for (size_t i = 1; i < size - idx; ++i) //找最大值或最小值
 		{			
 			if (arr[i] > find)
 			{
@@ -16,18 +17,18 @@ void SelectionSort(int arr[],size_t size)
 				maxpos = i;
 			}
 		}
-		if (maxpos != (size - idx - 1)) //�ж��ǲ���λ������ �Ͳ��ý���
+		if (maxpos != (size - idx - 1)) //判断是不是位置正好 就不用交换
 			std::swap(arr[maxpos],arr[size - idx -1]);
 	}
 }
 
 
-//�ҵ���С�����򣩻���󣨽��� �ŵ���ʼλ��
-void SelectionSort2(int arr[],size_t size) //����,����Сֵ�ŵ�ǰ��
+//找到最小（升序）或最大（降序） 放到开始位置
+void SelectionSort2(int arr[],size_t size) //升序,找最小值放到前面
 {
-	for (size_t i = 0; i < size - 1; ++i) //��һ������
+	for (size_t i = 0; i < size - 1; ++i) //少一次趟数
 	{
-		int pos = i; //���������Сֵ
+		int pos = i; //标记最大或最小值
 		for (size_t j = i + 1; j < size; ++j)
 		{
 			if (arr[pos] > arr[j])
@@ -39,7 +40,7 @@ void SelectionSort2(int arr[],size_t size) //����,����Сֵ�ŵ�ǰ��
 			std::swap(arr[i],arr[pos]);
 	}
 }
-// �Ż��汾��ͬʱ�ң��ҵ���Сֵ�����ֵ���ŵ���ʼ��ĩβ
+// 优化版本，同时找，找到最小值和最大值，放到起始和末尾
 void SelectionSort_P(int arr[],size_t size)
 {
 	int left = 0;
@@ -57,9 +58,9 @@ void SelectionSort_P(int arr[],size_t size)
 			if (arr[min] > arr[i])
 				min = i;
 		}
-//���������������1 ���ֵ-> left  && ���ֵ right ,ע�⽻��������
-//			      2 ���ֵ-> left || ��Сֵ right,ע�⽻����λ�ÿ��ܸı���
-// ����������������Ȱ���Сֵ����������ߣ��������������ֵ�Ļ����Ͱ���Сֵ��ֵ�����ֵ(��Ϊ��Сֵ��left������)
+//有两种特殊情况：1 最大值-> left  && 最大值 right ,注意交换了两次
+//			      2 最大值-> left || 最小值 right,注意交换后位置可能改变了
+// 处理这两种情况：先把最小值交换到最左边，如果最左边是最大值的话，就把最小值赋值给最大值(因为最小值和left交换了)
 //					
 		std::swap(arr[left], arr[min]);
 		if (left == max)
@@ -70,16 +71,16 @@ void SelectionSort_P(int arr[],size_t size)
 	}
 }
 
-//������: ����-> ��� ����->С��
-// ���� 1������
-//		2�ѶѶ� �����һλ����λ�ã�������
-void HeapAdjust(int arr[], int root, size_t size) //�Ӹ��ڵ����µ���
+//堆排序: 升序-> 大堆 降序->小堆
+// 升序 1创建堆
+//		2把堆顶 与最后一位交换位置，调整堆
+void HeapAdjust(int arr[], int root, size_t size) //从根节点向下调整
 {
 	size_t parent = root;
-	size_t child = 2 * parent + 1; //Ĭ�Ϻ�����СֵΪ����,�Һ��ӿ��ܲ�����
-	while (child < size)//���ڵ�ı䣬�������������������������
+	size_t child = 2 * parent + 1; //默认孩子最小值为左孩子,右孩子可能不存在
+	while (child < size)//根节点改变，可能左子树或右子树不满足堆
 	{
-		//�����ĺ���
+		//找最大的孩子
 		if (child + 1 < size && arr[child] < arr[child + 1])
 		{
 			child = child + 1;
@@ -95,18 +96,18 @@ void HeapAdjust(int arr[], int root, size_t size) //�Ӹ��ڵ����µ���
 			break;
 	}
 }
-void HeapSort(int arr[],size_t size) //����
+void HeapSort(int arr[],size_t size) //升序
 {
-	//1�����ѣ������һ����Ҷ�ӽڵ㿪ʼ����
+	//1创建堆，从最后一个非叶子节点开始调整
 	int idx = (size - 2) >> 1;
 	while (idx >= 0)
 	{
 		HeapAdjust(arr,idx,size);
 		idx--;
 	}
-	// �Ѷ������һ������
+	// 堆顶和最后一个交换
 	int last = size;
-	while (last > 1) //���ʣ��һ��Ԫ�أ����ý���
+	while (last > 1) //最后剩余一个元素，不用交换
 	{
 		std::swap(arr[0], arr[last -1]);
 		last--;
